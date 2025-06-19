fb5e77d970aa4bac3e60b23f5f6a9478b32c4485
Prasun Shrestha <shresthaprasun88@gmail.com>
2025-06-19 11:38:11 +0545

    Login

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -36,3 +36,63 @@ export const signup = async (req, res) => {
     console.log(error);
   }
 };
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Fields cannot be left empty!!",
+        success: false,
+      });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "Invalid User",
+        success: false,
+      });
+    }
+
+    const isPasswordMatched = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordMatched) {
+      return res.status(400).json({
+        message: "Incorrect Email or Password",
+        success: false,
+      });
+    }
+
+    const tokenData = {
+      userID: user._id,
+    };
+
+    const token = await jwt.sign(tokenData, process.env.JWT_SECRET_KEY, {
+      expiresIn: "1d",
+    });
+
+    const loggedInUser = {
+      _id: user._id,
+      fullname: user.fullname,
+      email: user.email,
+    };
+
+    return res
+      .status(201)
+      .cookie("token", token, {
+        maxAge: 1 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        sameSite: "strict",
+      })
+      .json({
+        message: `Welcome Back ${user.fullname}!!`,
+        success: true,
+        loggedInUser,
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { signup } from "../controllers/userController.js";
+import { login, signup } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.route("/signup").post(signup);
+
+router.route("/login").post(login);
 
 export default router;
