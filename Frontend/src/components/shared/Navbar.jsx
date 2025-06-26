352c1987f3e5008684a57de9a51f22add4fdec6d
Prasun Shrestha <shresthaprasun88@gmail.com>
2025-06-26 17:06:51 +0545

    Navbar

--- Frontend/src/components/shared/Navbar.jsx
+++ Frontend/src/components/shared/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -13,34 +14,48 @@ const Navbar = () => {
               viewBox="0 0 24 24"
               stroke="currentColor"
             >
-              {" "}
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth="2"
                 d="M4 6h16M4 12h16M4 18h7"
-              />{" "}
+              />
             </svg>
           </div>
           <ul
             tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-10 mt-3 w-52 p-2 shadow"
           >
             <li>
-              <a>Homepage</a>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive ? "text-primary font-medium" : ""
+                }
+              >
+                Home
+              </NavLink>
             </li>
             <li>
-              <a>Portfolio</a>
-            </li>
-            <li>
-              <a>About</a>
+              <NavLink
+                to="/about"
+                className={({ isActive }) =>
+                  isActive ? "text-primary font-medium" : ""
+                }
+              >
+                About
+              </NavLink>
             </li>
           </ul>
         </div>
       </div>
+
       <div className="navbar-center">
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <NavLink to="/" className="text-2xl font-bold">
+          TodoGenie
+        </NavLink>
       </div>
+
       <div className="navbar-end">
         <button className="btn btn-ghost btn-circle">
           <svg
@@ -50,13 +65,12 @@ const Navbar = () => {
             viewBox="0 0 24 24"
             stroke="currentColor"
           >
-            {" "}
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth="2"
               d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            />{" "}
+            />
           </svg>
         </button>
       </div>
