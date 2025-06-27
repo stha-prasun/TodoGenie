57b519723ec3eec0760425e53d15971a150d5446
Prasun Shrestha <shresthaprasun88@gmail.com>
2025-06-27 09:39:33 +0545

    Middleware

--- Backend/routes/todoRoute.js
+++ Backend/routes/todoRoute.js
@@ -1,20 +1,21 @@
 import express from "express";
 import { addTodo, deleteTodo, getAllTodos, getTodoById, markAsDone, search, updateTodo } from "../controllers/todoController.js";
+import isAuthenticated from "../middlewares/isAuthenticated.js";
 
 const router = express.Router();
 
-router.route("/new").post(addTodo);
+router.route("/new").post(isAuthenticated, addTodo);
 
-router.route("/mark").post(markAsDone);
+router.route("/mark").post(isAuthenticated, markAsDone);
 
-router.route("/delete").delete(deleteTodo);
+router.route("/delete").delete(isAuthenticated, deleteTodo);
 
-router.route("/update").post(updateTodo);
+router.route("/update").post(isAuthenticated, updateTodo);
 
-router.route("/get/all").post(getAllTodos);
+router.route("/get/all").post(isAuthenticated, getAllTodos);
 
-router.route("/get/:id").get(getTodoById);
+router.route("/get/:id").get(isAuthenticated, getTodoById);
 
-router.route("/search/:keyword").get(search);
+router.route("/search/:keyword").get(isAuthenticated, search);
 
 export default router;
