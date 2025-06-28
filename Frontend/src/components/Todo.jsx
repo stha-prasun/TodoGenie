1f746384346337c3f0bddaf48e530805942c1dd6
Prasun Shrestha <shresthaprasun88@gmail.com>
2025-06-28 13:40:45 +0545

    Delete API Call

--- Frontend/src/components/Todo.jsx
+++ Frontend/src/components/Todo.jsx
@@ -1,36 +1,36 @@
 import React from "react";
 import Navbar from "./shared/Navbar";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import useGetTodo from "../hooks/useGetTodo";
 import { useSelector } from "react-redux";
 import axios from "axios";
-import { AI_API_ENDPOINT } from "../utils/constants";
+import { AI_API_ENDPOINT, TODO_API_ENDPOINT } from "../utils/constants";
 import toast from "react-hot-toast";
 
 const Todo = () => {
   const { id } = useParams();
   const fetchTodo = useGetTodo(id);
 
+  const navigate = useNavigate();
+
   const todo = useSelector((store) => store.Todo.todo);
 
   const handleAI = async () => {
     try {
-      const response = await axios.post(
-        import.meta.env.VITE_AI_API_ENDPOINT,
-        {
-          contents: [
-            {
-              parts: [
-                {
-                  text: `You are an intelligent productivity assistant. Based on the task description provided, generate 3–5 smart, helpful suggestions that can help the user break down, improve, or approach the task more effectively. The suggestions should be Simple and clear, Actionable, Relevant to the task, Written in plain, helpful language. Task Description: ${todo?.description} Give your suggestions in a numbered list.`,
-                },
-              ],
-            },
-          ],
-        }
-      );
+      const response = await axios.post(import.meta.env.VITE_AI_API_ENDPOINT, {
+        contents: [
+          {
+            parts: [
+              {
+                text: `You are an intelligent productivity assistant. Based on the task description provided, generate 3–5 smart, helpful suggestions that can help the user break down, improve, or approach the task more effectively. The suggestions should be Simple and clear, Actionable, Relevant to the task, Written in plain, helpful language. Task Description: ${todo?.description} Give your suggestions in a numbered list.`,
+              },
+            ],
+          },
+        ],
+      });
 
-      const suggestion = response?.data?.candidates?.[0]?.content?.parts?.[0]?.text;
+      const suggestion =
+        response?.data?.candidates?.[0]?.content?.parts?.[0]?.text;
 
       const res = await axios.put(
         `${AI_API_ENDPOINT}/suggestion`,
@@ -48,13 +48,22 @@ const Todo = () => {
     }
   };
 
-  const handleDelete = async ()=>{
+  const handleDelete = async () => {
     try {
-      console.log("Delete");
+      const res = await axios.delete(`${TODO_API_ENDPOINT}/delete`, {
+        data: { id },
+        withCredentials: true,
+      });
+
+      if (res?.data?.success) {
+        toast.success(res.data.message);
+        navigate("/");
+      }
     } catch (error) {
       console.log(error);
+      toast.error(error.response.data.message);
     }
-  }
+  };
 
   return (
     <>
@@ -132,7 +141,12 @@ const Todo = () => {
                 🤖 Get AI Suggestion
               </button>
               <button className="btn btn-outline mt-4">Edit</button>
-              <button onClick={handleDelete} className="btn btn-outline btn-error mt-4">Delete</button>
+              <button
+                onClick={handleDelete}
+                className="btn btn-outline btn-error mt-4"
+              >
+                Delete
+              </button>
             </div>
             {todo?.aiSuggestion && (
               <div className="mt-4 p-4 bg-gray-100 rounded-xl border">
